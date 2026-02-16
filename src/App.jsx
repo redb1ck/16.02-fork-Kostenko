66c8d9af7a602a99dd011e4d551cc05aeff64ea5
Костенко Николай Павлович <29436770@s.mfua.ru>
2026-02-16 10:59:50 +0300

    Добавил бинарное дерево

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        width: "400px",
+        height: "400px",
+        border: "2px solid black",
+        margin: "20px auto",
+        textAlign: "center",
+        backgroundColor: "#f0f0f0"
+      }}
+    >
+      <h3>Бинарное дерево</h3>
+
+      <div>Root</div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          marginTop: "50px",
+        }}
+      >
+        <div>Left</div>
+        <div>Right</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
