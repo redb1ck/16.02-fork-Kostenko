4384c998a50b102fa343078f0e729a3d262c04a1
Костенко Николай Павлович <29436770@s.mfua.ru>
2026-02-16 11:55:07 +0300

    фибоначчи по центру,логика до 50

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,79 @@
-import './App.css'
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [result, setResult] = useState("");
+
+  function fibonacci(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+
+    return b;
+  }
+
+  function handleClick() {
+    const value = Number(number);
+
+    if (isNaN(value)) {
+      setResult("Введите число");
+      return;
+    }
+
+    if (value < 0) {
+      setResult("Введите положительное число");
+      return;
+    }
+
+    if (value > 50) {
+      setResult("Максимальное число 50");
+      return;
+    }
+
+    const fib = fibonacci(value);
+    setResult(fib);
+  }
+
   return (
     <div
       style={{
         width: "400px",
         height: "400px",
         border: "2px solid black",
-        margin: "20px auto",
+        margin: "50px auto",
+        padding: "20px",
         textAlign: "center",
-        backgroundColor: "#f0f0f0"
       }}
     >
-      <h3>Бинарное дерево</h3>
+      <h1>Числа Фибоначчи</h1>
 
-      <div>Root</div>
+      <input
+        type="number"
+        placeholder="Введите число"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        style={{ padding: "5px", marginBottom: "10px" }}
+      />
 
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          marginTop: "50px",
-        }}
-      >
-        <div>Left</div>
-        <div>Right</div>
-      </div>
+      <br />
+
+      <button onClick={handleClick} style={{ padding: "5px 15px" }}>
+        Посчитать
+      </button>
+
+      <h2 style={{ marginTop: "20px" }}>
+        Результат: {result}
+      </h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
